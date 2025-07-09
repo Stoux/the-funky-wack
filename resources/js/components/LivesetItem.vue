999a9044ff790aa8f34872d128d74555c15e4049
Leon Stam <leon@melonats.dev>
2025-07-09 14:06:29 +0200

    Add BPM & Genre to LivesetItem
    Closes #8, Closes #9

--- resources/js/components/LivesetItem.vue
+++ resources/js/components/LivesetItem.vue
@@ -46,7 +46,17 @@ const play = (quality?: keyof LivesetFilesByQuality) => {
                     <span class="text-muted-foreground" v-if="liveset.lineup_order !== null && liveset.lineup_order !== undefined">#{{ liveset.lineup_order }} &bull;</span>
                     {{ liveset.title }}
                 </h3>
-                <a href="#" class="text-sm text-primary hover:underline">{{ liveset.artist_name }}</a>
+                <div class="flex items-baseline space-x-2">
+                    <span class="text-sm text-primary">{{ liveset.artist_name }}</span>
+                    <template v-if="liveset.genre">
+                        <span class="text-muted-foreground text-xs">&bull;</span>
+                        <span class="text-muted-foreground text-xs" v-if="liveset.genre">{{ liveset.genre }}</span>
+                    </template>
+                    <template v-if="liveset.bpm">
+                        <span class="text-muted-foreground text-xs">&bull;</span>
+                        <span class="text-muted-foreground text-xs" v-if="liveset.genre">BPM: {{ liveset.bpm }}</span>
+                    </template>
+                </div>
             </div>
         </div>
 
